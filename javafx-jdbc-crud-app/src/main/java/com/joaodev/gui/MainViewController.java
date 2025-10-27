48e1a05f5b3faa6b1b3e5f05b4e1328a684a766d
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-10-27 17:04:10 -0300

    Implementa carregamento dinâmico de views no MainViewController
    Adiciona o método loadView ao MainViewController para carregar dinamicamente o conteúdo de arquivos FXML secundários dentro da janela principal, preservando a MenuBar.
    
    O método utiliza FXMLLoader para carregar o FXML solicitado.
    
    Acessa a Scene principal através de um método estático App.getMainScene() (a ser implementado/verificado na classe App).
    
    Manipula o Scene Graph para limpar o conteúdo do VBox principal, readicionar a MenuBar (assumida como primeiro filho) e adicionar os filhos do novo FXML carregado.
    
    O MenuItem "Sobre" (onMenuItemAboutAction) foi conectado para usar este novo método, carregando a view About.fxml.
    
    Inclui tratamento de IOException com um alerta visual para o usuário.

--- javafx-jdbc-crud-app/src/main/java/com/joaodev/gui/MainViewController.java
+++ javafx-jdbc-crud-app/src/main/java/com/joaodev/gui/MainViewController.java
@@ -1,11 +1,21 @@
 package com.joaodev.gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.joaodev.App;
+import com.joaodev.gui.util.Alerts;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -30,12 +40,31 @@ public class MainViewController implements Initializable {
 
        @FXML
     public void onMenuItemAboutAction(){
-        System.out.println("onMenuItemaboutAction");
+        loadView("/com/joaodev/gui/About.fxml");
     }
 
     @Override
     public void initialize(URL uri, ResourceBundle rb) {
         
+    }
+
+    private synchronized void loadView(String absoluteName) {
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVBox = loader.load();
+
+            Scene mainScene = App.getMainScene();
+            VBox mainVBox = (VBox) ((ScrollPane)mainScene.getRoot()).getContent();
+
+            Node mainMenu = mainVBox.getChildren().get(0);
+            mainVBox.getChildren().clear();
+            mainVBox.getChildren().add(mainMenu);
+            mainVBox.getChildren().addAll(newVBox.getChildren());
+
+        }
+        catch (IOException e){
+            Alerts.showAlert("IOException", "Erro de carregamento", e.getMessage(), AlertType.ERROR);
+        }
     }
     
 }
