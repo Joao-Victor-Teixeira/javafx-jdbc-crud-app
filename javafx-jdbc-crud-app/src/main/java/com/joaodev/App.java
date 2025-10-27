37803bde1c070b8f459311cb99b8b692b65041dc
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-10-27 15:43:28 -0300

    Carrega a view principal (MainView) via FXML
    Implementa o carregamento da interface gráfica principal da aplicação a partir do arquivo FXML (layout.fxml).
    
    A classe App.java foi atualizada para usar FXMLLoader para carregar /com/joaodev/gui/layout.fxml.
    
    O elemento raiz do FXML (ScrollPane) é corretamente carregado e configurado (fitToHeight, fitToWidth).
    
    A Scene principal é criada com o ScrollPane e exibida no Stage.
    
    O arquivo layout.fxml define a estrutura inicial da janela, contendo um VBox com a MenuBar (menus "Cadastro" e "Ajuda").
    
    Este passo estabelece a base da interface gráfica da aplicação JavaFX.

--- javafx-jdbc-crud-app/src/main/java/com/joaodev/App.java
+++ javafx-jdbc-crud-app/src/main/java/com/joaodev/App.java
@@ -1,30 +1,35 @@
 package com.joaodev;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
-    public static void main(String[] args) throws Exception {
-        launch(args);
-    }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("layout.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene tela = new Scene(root);
-
-        primaryStage.setTitle("Testando");
-        primaryStage.setScene(tela);
-        primaryStage.show();
+    @Override 
+    public void start(Stage primaryStage) { 
+         try { 
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/joaodev/gui/layout.fxml")); 
+            ScrollPane scrollPane = loader.load(); 
             
-        }
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
+            
+            Scene mainScene = new Scene(scrollPane); 
+            primaryStage.setScene(mainScene); 
+            primaryStage.setTitle("Sample JavaFX application"); 
+            primaryStage.show(); 
+    } 
+    catch (IOException e) { 
+            e.printStackTrace(); 
+        } 
+    } 
 
-    public static void setRoot(String string) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setRoot'");
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
